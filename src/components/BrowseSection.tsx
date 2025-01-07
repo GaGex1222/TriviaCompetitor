e44d88b6fae3643cb7574dd61b500d505f37c2e6
Gal <gald12123434@gmail.com>
2025-01-07 04:16:37 +0200

    Fixed more bugs for deplyoment

--- src/components/BrowseSection.tsx
+++ src/components/BrowseSection.tsx
@@ -5,6 +5,7 @@ import { useSearchParams } from "next/navigation";
 import { Trivia } from '@/interfaces/trivia';
 import { TriviaCard } from './TriviaCard';
 import { Loader2 } from "lucide-react"
+import { Suspense } from 'react';
 
 
 export default function BrowseSection(){
@@ -63,41 +64,43 @@ export default function BrowseSection(){
 
   return (
     <>
-      <div className="flex flex-col justify-center items-center h-auto mt-3">
-        <div className="bg-gray-900 p-8 shadow-xl rounded-lg w-full max-w-7xl">
-          <h2 className="text-4xl text-indigo-400 font-semibold text-center mb-8">Browse Quizzes</h2>
-          <div className="grid gap-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 min-h-[20rem]">
-            {triviasData ? (
-              triviasData.map((trivia) => (
-                <TriviaCard key={trivia.id} trivia={trivia} />
-              ))
-            ) : (
-              <div className="col-span-full flex justify-center items-center">
-                <Loader2 className="animate-spin w-12 h-12 text-indigo-400" />
-              </div>
-            )}
-          </div>
-          <div className="flex justify-between items-center mt-8">
-            <button
-              onClick={handlePreviousPage}
-              className="bg-indigo-600 text-white py-2 px-4 rounded transition-all duration-300 hover:bg-indigo-700 disabled:opacity-50"
-              disabled={page === 1}
-            >
-              Previous
-            </button>
-            <p className="text-center text-indigo-600 text-xl flex-grow mx-4">
-              {triviasData ? page : ''}
-            </p>
-            <button
-              disabled={triviasData ? triviasData.length < 8 : false}
-              onClick={handleNextPage}
-              className="bg-indigo-600 text-white py-2 px-4 rounded transition-all duration-300 hover:bg-indigo-700 disabled:opacity-50"
-            >
-              Next
-            </button>
+      <Suspense>
+        <div className="flex flex-col justify-center items-center h-auto mt-3">
+          <div className="bg-gray-900 p-8 shadow-xl rounded-lg w-full max-w-7xl">
+            <h2 className="text-4xl text-indigo-400 font-semibold text-center mb-8">Browse Quizzes</h2>
+            <div className="grid gap-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 min-h-[20rem]">
+              {triviasData ? (
+                triviasData.map((trivia) => (
+                  <TriviaCard key={trivia.id} trivia={trivia} />
+                ))
+              ) : (
+                <div className="col-span-full flex justify-center items-center">
+                  <Loader2 className="animate-spin w-12 h-12 text-indigo-400" />
+                </div>
+              )}
+            </div>
+            <div className="flex justify-between items-center mt-8">
+              <button
+                onClick={handlePreviousPage}
+                className="bg-indigo-600 text-white py-2 px-4 rounded transition-all duration-300 hover:bg-indigo-700 disabled:opacity-50"
+                disabled={page === 1}
+              >
+                Previous
+              </button>
+              <p className="text-center text-indigo-600 text-xl flex-grow mx-4">
+                {triviasData ? page : ''}
+              </p>
+              <button
+                disabled={triviasData ? triviasData.length < 8 : false}
+                onClick={handleNextPage}
+                className="bg-indigo-600 text-white py-2 px-4 rounded transition-all duration-300 hover:bg-indigo-700 disabled:opacity-50"
+              >
+                Next
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      </Suspense>
     </>
   );
 }
