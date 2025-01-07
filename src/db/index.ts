e37f9141e404035c1667dd783d160900b47bcb20
Gal <gald12123434@gmail.com>
2025-01-07 04:06:27 +0200

    Fixed more bugs (im tired)

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,8 +4,8 @@ import { createClient } from '@libsql/client';
 import * as schema from "./schema"
 
 const client = createClient({ 
-  url: process.env.TURSO_CONNECTION_URL!, 
-  authToken: process.env.TURSO_AUTH_TOKEN!
+  url: process.env.VITE_TURSO_CONNECTION_URL!, 
+  authToken: process.env.VITE_TURSO_AUTH_TOKEN!
 });
 
 export const db = drizzle({ client, schema });
